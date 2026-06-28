3c613b4b4ce9f76a7768227f87bd841b2a95a5fd
andrew3stedall <andrew3stedall@hotmail.com>
2026-06-29 08:40:46 +1000

    Expand typed PST primitives

--- src/pst/primitives.rs
+++ src/pst/primitives.rs
@@ -1,8 +1,71 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct NodeId(pub u64);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct BlockId(pub u64);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct ByteOffset(pub u64);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct PageRef {
+    pub offset: ByteOffset,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct BlockRef {
+    pub block_id: BlockId,
+    pub offset: ByteOffset,
+    pub size: u64,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PstVariant {
+    Unicode,
+    Ansi,
+    Unknown,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct RootPointers {
+    pub bbt_root: Option<PageRef>,
+    pub nbt_root: Option<PageRef>,
+}
+
+impl fmt::Debug for NodeId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "NodeId(0x{:x})", self.0)
+    }
+}
+
+impl fmt::Debug for BlockId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "BlockId(0x{:x})", self.0)
+    }
+}
+
+impl fmt::Debug for ByteOffset {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "ByteOffset({})", self.0)
+    }
+}
+
+impl fmt::Debug for PageRef {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("PageRef")
+            .field("offset", &self.offset)
+            .finish()
+    }
+}
+
+impl fmt::Debug for BlockRef {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BlockRef")
+            .field("block_id", &self.block_id)
+            .field("offset", &self.offset)
+            .field("size", &self.size)
+            .finish()
+    }
+}
