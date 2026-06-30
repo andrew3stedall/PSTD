5199acf02c922e0311a1c22a2a4c8f8c66a4227d
andrew3stedall <andrew3stedall@hotmail.com>
2026-06-30 22:49:46 +1000

    Address simple Clippy test warnings

--- tests/m3.rs
+++ tests/m3.rs
@@ -1,16 +1,16 @@
-use pstd::pst::heap::HeapOnNode;
 use pstd::pst::bth::BthMap;
+use pstd::pst::heap::HeapOnNode;
 
 #[test]
 fn heap_smoke() {
-    let mut bytes = vec![];
+    let mut bytes = Vec::new();
     bytes.extend_from_slice(&1u16.to_le_bytes());
     bytes.extend_from_slice(&2u16.to_le_bytes());
     bytes.extend_from_slice(&0u32.to_le_bytes());
     bytes.extend_from_slice(&0u16.to_le_bytes());
     bytes.extend_from_slice(&0u16.to_le_bytes());
     let heap = HeapOnNode::parse(&bytes, 0).unwrap();
-    assert_eq!(heap.allocations.len(), 0);
+    assert!(heap.allocations.is_empty());
 }
 
 #[test]
@@ -19,5 +19,5 @@ fn bth_smoke() {
     bytes.extend_from_slice(&0u16.to_le_bytes());
     bytes.extend_from_slice(&0u32.to_le_bytes());
     let bth = BthMap::parse(&bytes, 0).unwrap();
-    assert_eq!(bth.entries.len(), 0);
+    assert!(bth.entries.is_empty());
 }
