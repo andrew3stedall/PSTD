33e019e0175a330aed2fdb64037e1c2909dfd9bb
andrew3stedall <andrew3stedall@hotmail.com>
2026-07-02 06:25:32 +1000

    Format bounded node traversal condition

--- src/pst/nbt.rs
+++ src/pst/nbt.rs
@@ -181,7 +181,9 @@ impl NbtIndex {
             if parsed.is_internal() {
                 for child in parsed.child_page_refs {
                     discovered_child_pages += 1;
-                    if child.offset.0 < reader.file_size() && !seen_offsets.contains(&child.offset.0) {
+                    if child.offset.0 < reader.file_size()
+                        && !seen_offsets.contains(&child.offset.0)
+                    {
                         queue.push_back(PageRef {
                             offset: child.offset,
                         });
