be7f7c41e033fcb42cd9cda9a0080cfc1b698c3c
andrew3stedall <andrew3stedall@hotmail.com>
2026-07-05 17:14:18 +1000

    Expose index page diagnostics in inspect output

--- src/pst/inspect.rs
+++ src/pst/inspect.rs
@@ -1,9 +1,9 @@
 use std::path::Path;
 
 use crate::error::PstdResult;
-use crate::pst::bbt::BbtIndex;
+use crate::pst::bbt::{BbtIndex, BbtPageDiagnostic};
 use crate::pst::header::PstHeader;
-use crate::pst::nbt::NbtIndex;
+use crate::pst::nbt::{NbtIndex, NbtPageDiagnostic};
 use crate::pst::reader::PstByteReader;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -14,14 +14,16 @@ pub struct InspectSummary {
     pub root_diagnostic_condition: String,
     pub bbt_status: String,
     pub bbt_entries: usize,
+    pub bbt_page_diagnostics: Vec<BbtPageDiagnostic>,
     pub nbt_status: String,
     pub nbt_entries: usize,
+    pub nbt_page_diagnostics: Vec<NbtPageDiagnostic>,
 }
 
 impl InspectSummary {
     pub fn to_human_text(&self) -> String {
         format!(
-            "PSTD inspect\ninput: {}\nfile_size: {}\nformat: {}\nvariant: {}\nversion: {:?}\nheader_status: {}\nroot_diagnostic_condition: {}\nroot_selected_source: {:?}\nroot_candidate_count: {}\nbbt_status: {}\nbbt_entries: {}\nnbt_status: {}\nnbt_entries: {}",
+            "PSTD inspect\ninput: {}\nfile_size: {}\nformat: {}\nvariant: {}\nversion: {:?}\nheader_status: {}\nroot_diagnostic_condition: {}\nroot_selected_source: {:?}\nroot_candidate_count: {}\nbbt_status: {}\nbbt_entries: {}\nbbt_pages_diagnosed: {}\nnbt_status: {}\nnbt_entries: {}\nnbt_pages_diagnosed: {}",
             self.input_path,
             self.file_size,
             self.header.format,
@@ -33,8 +35,10 @@ impl InspectSummary {
             self.header.root_diagnostics.candidate_count,
             self.bbt_status,
             self.bbt_entries,
+            self.bbt_page_diagnostics.len(),
             self.nbt_status,
             self.nbt_entries,
+            self.nbt_page_diagnostics.len(),
         )
     }
 }
@@ -44,14 +48,16 @@ pub fn inspect_pst(input: impl AsRef<Path>) -> PstdResult<InspectSummary> {
     let header = PstHeader::parse(&reader)?;
     let root_diagnostic_condition = header.summary.root_diagnostics.condition.clone();
 
-    let (bbt_status, bbt_entries) = match BbtIndex::load_root(&reader, header.roots.bbt_root) {
-        Ok(bbt) => (bbt.status, bbt.entries.len()),
-        Err(err) => (format!("unavailable: {err}"), 0),
-    };
-    let (nbt_status, nbt_entries) = match NbtIndex::load_root(&reader, header.roots.nbt_root) {
-        Ok(nbt) => (nbt.status, nbt.entries.len()),
-        Err(err) => (format!("unavailable: {err}"), 0),
-    };
+    let (bbt_status, bbt_entries, bbt_page_diagnostics) =
+        match BbtIndex::load_root(&reader, header.roots.bbt_root) {
+            Ok(bbt) => (bbt.status, bbt.entries.len(), bbt.page_diagnostics),
+            Err(err) => (format!("unavailable: {err}"), 0, Vec::new()),
+        };
+    let (nbt_status, nbt_entries, nbt_page_diagnostics) =
+        match NbtIndex::load_root(&reader, header.roots.nbt_root) {
+            Ok(nbt) => (nbt.status, nbt.entries.len(), nbt.page_diagnostics),
+            Err(err) => (format!("unavailable: {err}"), 0, Vec::new()),
+        };
 
     Ok(InspectSummary {
         input_path: reader.input_path().display().to_string(),
@@ -60,7 +66,9 @@ pub fn inspect_pst(input: impl AsRef<Path>) -> PstdResult<InspectSummary> {
         root_diagnostic_condition,
         bbt_status,
         bbt_entries,
+        bbt_page_diagnostics,
         nbt_status,
         nbt_entries,
+        nbt_page_diagnostics,
     })
 }
